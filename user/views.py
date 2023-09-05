08e2fdb0ffcb129db7df88915011ad1077205455
Murilo Santori Boff <murilosantori45@gmail.com>
2023-09-05 20:37:12 -0300

    Cadastro

--- user/views.py
+++ user/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.messages import constants
+from django.contrib.auth.models import User # Criar o Cadastro
+from django.contrib.auth import authenticate # Autenticar Login
+from django.contrib.auth.decorators import login_required # Permissão de login para areas diversas do sistema
 
-# Autenticar o user!!!!!!!!!!!!!!!!!!
 
 def login(request):
     if request.method == 'GET':
@@ -14,7 +16,7 @@ def login(request):
         if len(email.strip()) == 0 or len(password.strip()) == 0:
             messages.add_message(request, constants.ERROR, 'Não existem dados o suficiente no formulario')
             return redirect('/user/login/')
-
+        
 def cadastro(request):
     if request.method == 'GET':
         return render(request, 'user/cadastro.html')
@@ -22,7 +24,15 @@ def cadastro(request):
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
-
         if len(username.strip()) == 0 or len(email.strip()) == 0 or len(password.strip()) == 0:
             messages.add_message(request, constants.ERROR, 'Não existem dados o suficiente no formulario')
             return redirect('/user/cadastro/')
+        else:
+            try:
+                user = User.objects.create_user(username, email, password)
+                user.save()
+                messages.add_message(request, constants.SUCCESS, 'Sucesso ao cadastrar Usuario')
+                return redirect('/user/login/')
+            except:
+                messages.add_message(request, constants.ERROR, 'Erro na Hora de Cadastrar')
+                return redirect('/user/cadastro/')
