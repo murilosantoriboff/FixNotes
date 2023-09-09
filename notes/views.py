e822aa6bb60705bfa6c40c9aa4357f51ed16a40a
Murilo Santori Boff <murilosantori45@gmail.com>
2023-09-09 07:57:26 -0300

    Planning next step

--- notes/views.py
+++ notes/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.decorators import login_required
 
 #Fazer o Log out futuramente
 
+# Fazer a interface para mostrar se ele possui notas ou nao, com as funções de criar categoria de notas e dentro dessas categorias criar notas, além de notas fora da categoria.
+
 @login_required(login_url='/user/login/')
 def home(request):
     return HttpResponse('Home')
