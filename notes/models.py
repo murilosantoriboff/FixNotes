a6cbc320176507b33f4849d49c6e283aa4155da4
Murilo Santori Boff <murilosantori45@gmail.com>
2023-09-22 20:03:04 -0300

    making the models on DBs

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    titulo = models.CharField(max_length=200)
+    descricao = models.TextField()
+
+    def __str__(self) -> str:
+        return self.titulo
+    
+class Nota(models.Model):
+    titulo_nota = models.CharField(max_length=200)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    desc_nota = models.TextField()
+
+    def __str__(self) -> str:
+        return self.titulo_nota
