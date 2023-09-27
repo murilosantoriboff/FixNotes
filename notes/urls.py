04d47c46fbb879e0c01c169220bcc966a096de7d
Murilo Santori Boff <murilosantori45@gmail.com>
2023-09-27 20:03:45 -0300

    half of the project

--- notes/urls.py
+++ notes/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('nova_categoria/', views.nova_categoria, name='nova_categoria'),
+    path('nova_nota', views.nova_nota, name='nova_nota')
 ]
